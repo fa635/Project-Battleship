4f5905bd4816abf20edcb1f0c959f60f9141f131
fa635 <bfalonne823@gmail.com>
2024-05-03 12:09:18 +0200

    Move missedShots to constructor and make ship sink
    When located in the receiveAttack function, the missedShots array would
    get empty at every new function call

--- gameboard.js
+++ gameboard.js
@@ -2,6 +2,7 @@ class Gameboard {
     constructor (player) {
         this.player = player
         this.board = []
+        this.missedShots = []
     }
 
     placeShip (ship, ...coordinates) {
@@ -10,8 +11,34 @@ class Gameboard {
         return this.board[this.board.length - 1]
     }
 
-    // this is stupid, the ship's gonna take a bunch of places not 1 cell
+    receiveAttack (coordinate) {
+        
+
+        let attackHitsShip = false
+
+        for (let i = 0;  i < this.board.length; i++ ) {
+            if (this.board[i].shipPlacement.includes(coordinate)) {
+                this.board[i].shipType.hit()
+
+                attackHitsShip = true
+
+                if (this.board[i].shipType.numberOfHits === this.board[i].shipType.shipLength) {
+                    this.board[i].shipType.shipIsSunk = true
+                } 
+
+                return this.board[i].shipType
+            }
+        }
+
+        if (attackHitsShip === false) {
+
+            this.missedShots.push(coordinate)
+
+            return this.missedShots
+        } 
+    }
 }
+
 
 
 module.exports = Gameboard
--- gameboard.test.js
+++ gameboard.test.js
@@ -19,3 +19,38 @@ describe("Gameboard.placeShip", () => {
         expect(player1.placeShip(destroyer, 11, 12)).toEqual({"shipPlacement": [11, 12], "shipType": {"numberOfHits": 0, "shipIsSunk": false, "shipLength": 2}})
     })
 })
+
+describe("Gameboard.receiveAttack", () => {
+    test("hit ship but the ship doesn't sink", () => {
+        expect(player1.receiveAttack(1)).toEqual({"numberOfHits": 1, "shipIsSunk": false, "shipLength": 5})
+    })
+    test("hit ship but the ship doesn't sink", () => {
+        expect(player1.receiveAttack(11)).toEqual({"numberOfHits": 1, "shipIsSunk": false, "shipLength": 2})
+    })
+    test("hit ship but the ship doesn't sink", () => {
+        expect(player1.receiveAttack(8)).toEqual({"numberOfHits": 1, "shipIsSunk": false, "shipLength": 3})
+    })
+    test("hit ship but the ship doesn't sink", () => {
+        expect(player1.receiveAttack(9)).toEqual({"numberOfHits": 2, "shipIsSunk": false, "shipLength": 3})
+    })
+
+
+
+    test("doesn't hit any ship 6", () => {
+        expect(player1.receiveAttack(6)).toEqual([6])
+    })
+    test("doesn't hit any ship 7", () => {
+        expect(player1.receiveAttack(7)).toEqual([6, 7])
+    })
+    test("doesn't hit any ship 13", () => {
+        expect(player1.receiveAttack(13)).toEqual([6, 7, 13])
+    })
+
+
+    test("hit ship and the ship is sunk", () => {
+        expect(player1.receiveAttack(12)).toEqual({"numberOfHits": 2, "shipIsSunk": true, "shipLength": 2})
+    })
+    test("hit ship and the ship is sunk", () => {
+        expect(player1.receiveAttack(10)).toEqual({"numberOfHits": 3, "shipIsSunk": true, "shipLength": 3})
+    })
+})
